f84b2fb839967bf99736df5f7ef17491545519bf
Modernize Studio <studio@modernize.com>
2025-11-25 14:12:13 +0000

    Commit for planId: 379

--- src/main/java/crm/entity/Customer.java
+++ src/main/java/crm/entity/Customer.java
@@ -5,11 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.util.Set;
 
 @Entity
